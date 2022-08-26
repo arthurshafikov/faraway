3887394e6f21ed5ba604ce1354c0230197cfbd0d
arthurshafikov <arthur.shafikov27@gmail.com>
2022-08-27 01:09:19 +0400

    init client

---
+++ client/cmd/app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/arthurshafikov/faraway/client/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ client/go.mod
@@ -0,0 +1,3 @@
+module github.com/arthurshafikov/faraway/client
+
+go 1.18
