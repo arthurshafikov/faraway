d396f48dde79d910bc35e236163f4eed45f2d4c4
arthurshafikov <arthur.shafikov27@gmail.com>
2022-08-26 13:30:36 +0400

    add GetQuote unit test

---
+++ server/internal/services/quote_test.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	quotes         = []byte("someQuote1\nsomeQuote2\nsomeQuote3")
+	quotesFilePath = "quotes.txt"
+	expectedQuotes = []string{"someQuote1", "someQuote2", "someQuote3"}
+)
+
+func TestGetQuote(t *testing.T) {
+	createFakeQuotesFile(t)
+	quoteService := NewQuoteService(quotesFilePath)
+
+	var resultQuotes [][]byte
+	for i := 0; i < 10; i++ {
+		resultQuotes = append(resultQuotes, quoteService.GetQuote())
+	}
+
+	for _, resultQuote := range resultQuotes {
+		require.Contains(t, expectedQuotes, string(resultQuote))
+	}
+	deleteFakeQuotesFile(t)
+}
+
+func createFakeQuotesFile(t *testing.T) {
+	t.Helper()
+	if err := os.WriteFile(quotesFilePath, quotes, 0644); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func deleteFakeQuotesFile(t *testing.T) {
+	t.Helper()
+	if err := os.Remove(quotesFilePath); err != nil {
+		t.Fatal(err)
+	}
+}
